31129bf6a57f214ee204ed85705fa3d1141fa10a
Stefan Leijnen <stefan@leijnen.com>
2014-02-27 18:37:22 +0100

    small changes

--- first.js
+++ first.js
@@ -1,5 +1,6 @@
 
 var apiKey      = "7261e136fa4f72fbbabe8057a904dba1";
 var tenantId    = "AILABS_DEMO";
+var userId      = "Carl";
 
 function log(text) {document.getElementById("log").innerHTML += (text + "<br>")}
