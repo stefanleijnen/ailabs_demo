16aa28ea80e0488fe863f7ad7e45cd808f38b652
Geert Jonker XPS <gmjonker@gmail.com>
2014-02-27 16:15:39 +0100

    eerste pagina aanzet

---
+++ dummy-data.js
@@ -0,0 +1,11 @@
+var dummydata = 
+{
+	"di1": 
+		{	userId:"",
+            itemId:"di1",
+            itemUrl:"http://example.com/item/1",
+            itemDescription:"Easyrec Fan Poster 1",
+            itemImageUrl:"http://example.com/pics/poster1.png"
+		}
+}
+		
