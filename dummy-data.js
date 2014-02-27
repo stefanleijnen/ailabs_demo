b7b68e9dc1ca141cebeaf0d96bb42b462eb4c138
Stefan Leijnen <stefan@leijnen.com>
2014-02-27 16:56:46 +0100

    dummy data deels aangevuld

--- dummy-data.js
+++ dummy-data.js
@@ -1,11 +1,30 @@
 var dummydata = 
 {
-	"di1": 
-		{	userId:"",
-            itemId:"di1",
-            itemUrl:"http://example.com/item/1",
-            itemDescription:"Easyrec Fan Poster 1",
-            itemImageUrl:"http://example.com/pics/poster1.png"
+	"huis1388":
+		{	userId:"demo_user1",
+            itemId:"huis1388",
+            itemUrl:"http://www.covershousing.com/property/1388/achter-clarenburg/",
+            itemDescription:"Achter Clarenburg",
+            itemImageUrl:"http://www.covershousing.com/images_huis/1/1388-DSC04973.JPG"
+            itemCity:"Utrecht",
+            itemArea:"City Center",
+            itemFurnishing:"Furnished",
+            itemBedrooms:"1",
+            itemSize:"70 m2",
 		}
+
+	"huis1388":
+    		{	userId:"demo_user1",
+                itemId:"huis1388",
+                itemUrl:"http://www.covershousing.com/property/1388/achter-clarenburg/",
+                itemDescription:"Achter Clarenburg",
+                itemImageUrl:"http://www.covershousing.com/images_huis/1/1388-DSC04973.JPG"
+                itemCity:"Utrecht",
+                itemArea:"City Center",
+                itemFurnishing:"Furnished",
+                itemBedrooms:"1",
+                itemSize:"70 m2",
+    		}
+
 }
-		
+
